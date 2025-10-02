e0b818b9af06608737d087a2b501389f574fdce8
SoiniN <nis00085@laurea.fi>
2025-10-02 12:31:46 +0300

    WS4

---
+++ Workshop 4/ws4.js
@@ -0,0 +1,84 @@
+function changeHeading()
+{
+    document.getElementById("otsikko").innerHTML="Muokattu otsikko !"
+}
+
+function secondButton()
+{
+    let heading = document.getElementById('otsikko2');
+    heading.style.fontSize="32px";
+    heading.style.color="blue";
+    heading.style.fontstyle="Arial"
+}
+function thirdButton()
+{
+    document.querySelectorAll("em")[0].innerHTML="Vieläkö on villihevosia"
+    document.querySelectorAll("em")[1].innerHTML="Ja vieläkö jossain tikolainen"
+}
+
+function showSelection() {
+    var select = document.getElementById("mySelect");
+    var value = select.value;
+    alert("Valitsit " + value);
+
+    var img = document.getElementById("carimage");
+
+    if (value === "BMW") {
+        img.src = "https://www.bmw.fi/content/dam/bmw/common/all-models/m-series/m8-coupe/2022/navigation/bmw-8series-coupe-modellfinder.png";
+    } else if (value === "Audi") {
+        img.src = "https://mediaservice.audi.com/media/fast/H4sIAAAAAAAAAFvzloG1tIiBOTrayfuvpGh6-m1zJgaGigIGBgZGoDhTtNOaz-I_2DhCHsCEtzEwF-SlMwJZKUycmbmJ6an6QD4_I3taTmV-aUkxO0grz5x432LHxE3WfU0dkTsecZz-O6OhjoEVqItxFZBgXgIk-LYDCU5XBjAJMm8NiKgE8ZmsmRkYWCuAjEgGEODjKy3KKUgsSszVK89MKckQ1DAgEgizu7iGOHr6BAMAYiN-0OkAAAA";
+    } else if (value === "Mercedes") {
+        img.src = "https://dsu2f16ugr2z8.cloudfront.net/1600x1200/autotalliprodmedia/images/netweels/Mercedes-Benz/SL/2022/3faa02e6-2c1e-467b-a420-e70a55fd7abe.jpg";
+    } else if (value === "Volvo") {
+        img.src = "https://wizz.volvocars.com/images/2026/246/exterior/studio/standardizedSideView/transparent_exterior-studio-standardizedSideView_B6AF02715D08007BDC58547AA6FB3FA625421726.png?client=site-navigation&w=3840&imdensity=1";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    var img = document.getElementById("carimage");
+    img.addEventListener("mouseover", function() {
+        img.style.border = "5px solid red";
+    });
+    img.addEventListener("mouseout", function() {
+        img.style.border = "none";
+    });
+});
+
+function insertRow() {
+  var nimi = document.getElementById('nimi').value.trim();
+  var teht = document.getElementById('tehtava').value.trim();
+  var palk = document.getElementById('palkka').value.trim();
+
+  var virheet = [];
+  if (nimi.length <= 5) virheet.push('Nimen pitää olla yli 5 merkkiä.');
+  if (teht === '') virheet.push('Tehtävä ei saa olla tyhjä.');
+  if (!/^\d+(\.\d+)?$/.test(palk) || Number(palk) <= 0) virheet.push('Palkka pitää olla numero > 0.');
+
+  if (virheet.length > 0) {
+    alert('Korjaa:\n' + virheet.join('\n'));
+    return;
+  }
+
+  var table = document.getElementById('data');
+  var tbody = table.tBodies[0];
+  if (!tbody) tbody = table.appendChild(document.createElement('tbody'));
+
+  var r = tbody.insertRow();
+  r.insertCell(0).textContent = nimi;
+  r.insertCell(1).textContent = teht;
+  r.insertCell(2).textContent = palk + ' €';
+
+  var c4 = r.insertCell(3);
+  var btn = document.createElement('button');
+  btn.type = 'button';
+  btn.textContent = 'Poista';
+  btn.onclick = function() { this.parentNode.parentNode.remove(); };
+  c4.appendChild(btn);
+
+  document.getElementById('nimi').value = '';
+  document.getElementById('tehtava').value = '';
+  document.getElementById('palkka').value = '';
+}
+
+var insertBtn = document.getElementById('Insert');
+if (insertBtn) insertBtn.onclick = insertRow;
